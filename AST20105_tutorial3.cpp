4c8006aa7b86d9cca8f07ff423c719beed281f5e
Kinson1012 <kinsonleung11@gmail.com>
2023-09-17 20:27:33 +0800

    finished tutorial 3

--- AST20105_tutorial3.cpp
+++ AST20105_tutorial3.cpp
@@ -5,67 +5,118 @@
 #include <cmath>
 using namespace std;
 
-
-double sub_num(int A, int B, int C, double x){
-    return (A*pow(x,2)+B*x+C);
+double sub_num(int A, int B, int C, double x)
+{
+    return (A * pow(x, 2) + B * x + C);
 }
 
-
-bool validation(double fx1, double fx2){
-    if((fx1>0 && fx2<0) || (fx1<0 && fx2 >0)){
-        return true;
-    }else{
-        return false;
+bool validation(double fx1, double fx2)
+{
+    bool check = false;
+    if (signbit(fx1) != signbit(fx2))
+    {
+        check = true;
     }
+    return check;
 }
 
-double inter_bisection(int A, int B, int C, double x1, double x2 , double fx1, double fx2){
+double inter_bisection(int A, int B, int C, double x1, double x2, double fx1, double fx2)
+{
     double m, fm;
-    m = (x1+x2)/2;
-    fm = sub_num(A,B,C,m);
-    cout << fx1 << " " << fx2 << " " << m << " " << fm << endl;
-    while(fx1!=0.00 || fx2!=0.00 || fm!=0.00 || fm>=0.01 || abs(fx1)-abs(fx2)>=0.01){
-    m = (x1+x2)/2;
-    fm = sub_num(A,B,C,m);
-    fx1 = sub_num(A,B,C,x1);
-    fx2 = sub_num(A,B,C,x2);
-    cout << fm << endl;
-    cout << fx1 << endl;
-    cout << fx2 << endl;
-    if(validation(fx1,fm)==1){
-        x1 = m;
-    }else if(validation(fx2,fm)==1){
-        x2 = m;
-    }
-    cout << "x1: " << x1 << " x2:" << x2 << endl;
-    }
-    if(sub_num(A,B,C,x1)==0){
+    do
+    {
+        m = (x1 + x2) / 2;
+        fm = sub_num(A, B, C, m);
+        fx1 = sub_num(A, B, C, x1);
+        fx2 = sub_num(A, B, C, x2);
+        if (validation(fx1, fm) != 1)
+        {
+            x1 = m;
+        }
+        else if (validation(fx2, fm) != 1)
+        {
+            x2 = m;
+        }
+        cout << "x1: " << x1 << " x2: " << x2 << endl;
+    } while (fx1 != 0 && fx2 != 0 && (abs(x1 - x2) > 0.01 || abs(fm) > 0.01) && fm != 0);
+    if (fx1 == 0)
+    {
         return x1;
-    }else if(sub_num(A,B,C,x2)==0){
+    }
+    else if (fx2 == 0)
+    {
         return x2;
     }
+    else if (fm == 0)
+    {
+        return m;
+    }
+    else if (abs(x1 - x2) < 0.01 || abs(fm) < 0.01)
+    {
+        return x1;
+    }
 }
 
+double recur_bisection(int A, int B, int C, double x1, double x2, double fx1, double fx2)
+{
+    double m, fm;
+    if (fx1 == 0)
+    {
+        return x1;
+    }
+    else if (fx2 == 0)
+    {
+        return x2;
+    }
+    else if (abs(x1 - x2) < 0.01 || abs(fm) < 0.01)
+    {
+        return x1;
+    }
+    else
+    {
+        m = (x1 + x2) / 2;
+        fm = sub_num(A, B, C, m);
+        if (validation(fx1, fm) != 1)
+        {
+            x1 = m;
+        }
+        else if (validation(fx2, fm) != 1)
+        {
+            x2 = m;
+        }
+        cout << "x1: " << x1 << " x2: " << x2 << endl;
+        fx1 = sub_num(A, B, C, x1);
+        fx2 = sub_num(A, B, C, x2);
+        return recur_bisection(A, B, C, x1, x2, fx1, fx2);
+    }
+}
 
-int main(){
+int main()
+{
     int A, B, C, d;
     double x1, x2, fx1, fx2;
     cout << "Ax^2 + Bx + C" << endl;
     cout << "Input values  of A, B and C: ";
     cin >> A >> B >> C;
-    d = pow(B,2) - 4*A*C;
+    d = pow(B, 2) - 4 * A * C;
     cout << "Discriminant: " << d << endl;
-    if(d>0){
+    if (d > 0)
+    {
         cout << "Input x1 and x2: ";
         cin >> x1 >> x2;
-        fx1 = sub_num(A,B,C,x1);
-        fx2 = sub_num(A,B,C,x2);
-        while(validation(fx1,fx2)!=1){
-            cout << "Invalid input, Please input again"<< endl;
+        fx1 = sub_num(A, B, C, x1);
+        fx2 = sub_num(A, B, C, x2);
+        while (validation(fx1, fx2) != 1)
+        {
+            cout << "Invalid input, Please input again" << endl;
             cout << "Input x1 and x2: ";
             cin >> x1 >> x2;
         }
-        cout << inter_bisection(A, B, C, x1, x2, fx1, fx2) << endl;
+        cout << "\nBisection (Interative) begin"
+             << "\n============================" << endl;
+        cout << "Root: " << inter_bisection(A, B, C, x1, x2, fx1, fx2) << endl;
+        cout << "\n\nBisection (Recursive) begin"
+             << "\n============================" << endl;
+        cout << "Root: " << recur_bisection(A, B, C, x1, x2, fx1, fx2) << endl;
     }
 }
-
