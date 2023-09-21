9809f29d5a406126b1a6b0e598a385ca602871c9
Kinson1012 <63942215+Kinson1012@users.noreply.github.com>
2023-09-21 13:30:08 +0800

    adding comments

--- AST20105_tutorial3.cpp
+++ AST20105_tutorial3.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 double sub_num(int A, int B, int C, double x)
-{
+{//function to calculate the value of the polynomial
     return (A * pow(x, 2) + B * x + C);
 }
 
 bool validation(double fx1, double fx2)
-{
+{//function to check if the two values are the same sign
     bool check = false;
     if (signbit(fx1) != signbit(fx2))
     {
@@ -22,10 +22,10 @@ bool validation(double fx1, double fx2)
 }
 
 double inter_bisection(int A, int B, int C, double x1, double x2, double fx1, double fx2)
-{
+{//function to find the root of the polynomial with interation
     double m, fm;
     do
-    {
+    {//loop to find root
         m = (x1 + x2) / 2;
         fm = sub_num(A, B, C, m);
         fx1 = sub_num(A, B, C, x1);
@@ -40,7 +40,7 @@ double inter_bisection(int A, int B, int C, double x1, double x2, double fx1, do
         }
         cout << "x1: " << x1 << " x2: " << x2 << endl;
     } while (fx1 != 0 && fx2 != 0 && (abs(x1 - x2) > 0.01 || abs(fm) > 0.01) && fm != 0);
-    if (fx1 == 0)
+    if (fx1 == 0)//returning root
     {
         return x1;
     }
@@ -60,7 +60,7 @@ double inter_bisection(int A, int B, int C, double x1, double x2, double fx1, do
 }
 
 double recur_bisection(int A, int B, int C, double x1, double x2, double fx1, double fx2)
-{
+{//function to find the root of the polynomial with recursive method
     double m, fm;
     fm = sub_num(A, B, C, m);
     if (fx1 == 0)
@@ -96,15 +96,19 @@ double recur_bisection(int A, int B, int C, double x1, double x2, double fx1, do
 
 int main()
 {
+    //declaring variables
     int A, B, C, d;
     double x1, x2, fx1, fx2;
+    //asking for input
     cout << "Ax^2 + Bx + C" << endl;
     cout << "Input values  of A, B and C: ";
     cin >> A >> B >> C;
+    //calculating discriminant
     d = pow(B, 2) - 4 * A * C;
     cout << "Discriminant: " << d << endl;
     if (d > 0)
     {
+        //validating input
         cout << "Input x1 and x2: ";
         cin >> x1 >> x2;
         fx1 = sub_num(A, B, C, x1);
@@ -115,6 +119,7 @@ int main()
             cout << "Input x1 and x2: ";
             cin >> x1 >> x2;
         }
+        //calling functions
         cout << "\nBisection (Interative) begin"
              << "\n============================" << endl;
         cout << "Root: " << inter_bisection(A, B, C, x1, x2, fx1, fx2) << endl;
